f8621e62012215c5cbf1c9392c428123df57de90
Alex Hofer <>
2019-12-09 15:50:39 +0100

    Fixes on Menus Work on End Screen

---
+++ Assets/Scripts/System/GameLoaderEndgame.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameLoaderEndgame : MonoBehaviour
+{
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void LoadMenu()
+    {
+        try
+        {
+            FindObjectOfType<PlayerData>().ResetData();
+            Destroy(FindObjectOfType<SystemProperties>().gameObject);
+            Destroy(FindObjectOfType<NoReload>().gameObject);
+        }
+        catch
+        {
+            print("object_not_found");
+        }
+        SceneManager.LoadScene("Start");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void DisplayCredits()
+    {
+        //TODO
+    }
+}
